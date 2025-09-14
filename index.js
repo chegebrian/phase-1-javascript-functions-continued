e29bf82e049aa179d2313e94a43858be6cb219c8
brianwaithaka <waithakabrian8@gmail.com>
2025-09-14 18:43:57 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(action = "roller-skate") {
+  return `This Saturday, I want to ${action}!`;
+}
+
+// console.log(saturdayFun());
+// console.log(saturdayFun("bathe my dog!"));
+
+// saturdayFun();
+saturdayFun("bathe my dog");
+
+const mondayWork = function(action = "go to the office"){
+    return `This Monday, I will ${action}.`
+}
+
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(special = "*"){
+return function(value = 'a hard worker') {
+    return `You are ${special}${value}${special}!`
+}
+}
+wrapAdjective();
+wrapAdjective("||")("a dedicated programmer")
